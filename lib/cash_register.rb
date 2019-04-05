c368f70924b01d9654f3c744b31b6670c84c9220
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-05 22:05:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0, discount = nil)
+    @total = total
+    @discount = 20
+  end
 
+  def add_item(item, price)
+
+  end
+
+
+
+end
